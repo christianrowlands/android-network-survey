d244ad1b1f84c75e6d6ea8ea88ea363ddbe8952f
Christian Rowlands <craxiomdev@gmail.com>
2019-09-12 16:27:31 -0400

    Migrated to AndroidX

--- networksurvey/src/main/java/com/craxiom/networksurvey/fragments/CalculatorFragment.java
+++ networksurvey/src/main/java/com/craxiom/networksurvey/fragments/CalculatorFragment.java
@@ -3,7 +3,7 @@ package com.craxiom.networksurvey.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
