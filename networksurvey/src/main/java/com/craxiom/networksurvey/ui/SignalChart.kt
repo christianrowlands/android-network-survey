244d861cb770d2ad9709c346a67235561daca1f7
christianrowlands <christian.rowlands@gmail.com>
2025-08-12 12:26:04 -0400

    Use the app primary color for the signal line on the chart

--- networksurvey/src/main/java/com/craxiom/networksurvey/ui/SignalChart.kt
+++ networksurvey/src/main/java/com/craxiom/networksurvey/ui/SignalChart.kt
@@ -3,14 +3,18 @@ package com.craxiom.networksurvey.ui
 import androidx.compose.animation.core.snap
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.colorResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.craxiom.networksurvey.R
 import com.patrykandpatrick.vico.compose.cartesian.CartesianChartHost
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberStart
 import com.patrykandpatrick.vico.compose.cartesian.layer.rememberLineCartesianLayer
 import com.patrykandpatrick.vico.compose.cartesian.rememberCartesianChart
 import com.patrykandpatrick.vico.compose.cartesian.rememberVicoScrollState
+import com.patrykandpatrick.vico.compose.common.fill
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianLayerRangeProvider
+import com.patrykandpatrick.vico.core.cartesian.layer.LineCartesianLayer
 
 /**
  * A chart that shows signal values (e.g. RSSI) over time.
@@ -36,6 +40,13 @@ private fun ComposeChart(viewModel: ASignalChartViewModel) {
     CartesianChartHost(
         chart = rememberCartesianChart(
             rememberLineCartesianLayer(
+                lineProvider = LineCartesianLayer.LineProvider.series(
+                    LineCartesianLayer.Line(
+                        fill = LineCartesianLayer.LineFill.single(
+                            fill(colorResource(id = R.color.colorAccent))
+                        )
+                    )
+                ),
                 rangeProvider = CartesianLayerRangeProvider.fixed(
                     minY = minRssi.toDouble(),
                     maxY = maxRssi.toDouble(),
